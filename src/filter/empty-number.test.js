c65b7b06db52c0458dab8cade7e3ba676703379f
Ed Rooth <ed.rooth@coreos.com>
2014-04-17 15:19:59 -0700

    feat(filer/empty-number): show a default value for databound numbers when empty

---
+++ src/filter/empty-number.test.js
@@ -0,0 +1,43 @@
+'use strict';
+
+describe('coreos.filters.coEmptyNumber', function() {
+  var coEmptyNumberFilter,
+      defaultText = '&ndash;';
+
+  // Load the module.
+  beforeEach(module('coreos.filters'));
+
+  // Initialize the controller and a mock scope
+  beforeEach(inject(function(_coEmptyNumberFilter_) {
+    coEmptyNumberFilter = _coEmptyNumberFilter_;
+  }));
+
+  it('returns default text when null', function() {
+    expect(coEmptyNumberFilter(null)).toBe(defaultText);
+  });
+
+  it('returns default text when undefined', function() {
+    expect(coEmptyNumberFilter(undefined)).toBe(defaultText);
+  });
+
+  it('returns default text when NaN', function() {
+    expect(coEmptyNumberFilter(NaN)).toBe(defaultText);
+  });
+
+  it('returns actual value when zero', function() {
+    expect(coEmptyNumberFilter(0)).toBe('0');
+  });
+
+  it('returns actual value when non-zero', function() {
+    expect(coEmptyNumberFilter(1)).toBe('1');
+  });
+
+  it('returns actual value when less than zero', function() {
+    expect(coEmptyNumberFilter(-1)).toBe('-1');
+  });
+
+  it('returns a custom default text', function() {
+    expect(coEmptyNumberFilter(null, 'custom')).toBe('custom');
+  });
+
+});
