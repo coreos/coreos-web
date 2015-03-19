77f41f3b56d19832a3a0e985b15889b8abdfe4ce
Ed Rooth <ed.rooth@coreos.com>
2015-03-19 15:22:26 -0700

    ui/cog: prevent click propagation. close dropdown after selection is made.

--- src/ui/cog/cog.js
+++ src/ui/cog/cog.js
@@ -18,12 +18,23 @@ angular.module('coreos.ui')
       'anchor': '@'
     },
     controller: function($scope) {
+      $scope.status = {
+        isopen: false,
+      };
+
+      // Capture all clicks on the cog to prevent bubbling.
+      $scope.captureClick = function($event) {
+        $event.stopPropagation();
+      };
+
+      // Handles dropdown item clicks.
       $scope.clickHandler = function($event, option) {
         $event.preventDefault();
         $event.stopPropagation();
         if (option.callback) {
           option.callback();
         }
+        $scope.status.isopen = false;
       };
     }
   };
